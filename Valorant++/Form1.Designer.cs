77984b7f8ee06e98ceaa3dcd59fd15f5a275fbda
gookie <lg23122002.lg@gmail.com>
2021-03-26 18:48:28 +0100

    changeable path added

--- Valorant++/Form1.Designer.cs
+++ Valorant++/Form1.Designer.cs
@@ -32,11 +32,17 @@ namespace Valorant__
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPageSettings = new System.Windows.Forms.TabPage();
+            this.txtValorantPath = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.tabPageLogs = new System.Windows.Forms.TabPage();
             this.txtBoxLogs = new System.Windows.Forms.TextBox();
             this.tabPageClientApi = new System.Windows.Forms.TabPage();
             this.txtBoxClientApi = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.tabControl1.SuspendLayout();
+            this.tabPageSettings.SuspendLayout();
             this.tabPageLogs.SuspendLayout();
             this.tabPageClientApi.SuspendLayout();
             this.SuspendLayout();
@@ -48,27 +54,70 @@ namespace Valorant__
             this.tabControl1.Controls.Add(this.tabPageClientApi);
             this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(6);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(469, 526);
+            this.tabControl1.Size = new System.Drawing.Size(938, 1012);
             this.tabControl1.TabIndex = 1;
             // 
             // tabPageSettings
             // 
-            this.tabPageSettings.Location = new System.Drawing.Point(4, 22);
+            this.tabPageSettings.Controls.Add(this.txtValorantPath);
+            this.tabPageSettings.Controls.Add(this.label3);
+            this.tabPageSettings.Controls.Add(this.label2);
+            this.tabPageSettings.Controls.Add(this.label1);
+            this.tabPageSettings.Location = new System.Drawing.Point(8, 39);
+            this.tabPageSettings.Margin = new System.Windows.Forms.Padding(6);
             this.tabPageSettings.Name = "tabPageSettings";
-            this.tabPageSettings.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPageSettings.Size = new System.Drawing.Size(520, 484);
+            this.tabPageSettings.Padding = new System.Windows.Forms.Padding(6);
+            this.tabPageSettings.Size = new System.Drawing.Size(922, 965);
             this.tabPageSettings.TabIndex = 1;
             this.tabPageSettings.Text = "Settings";
             this.tabPageSettings.UseVisualStyleBackColor = true;
             // 
+            // txtValorantPath
+            // 
+            this.txtValorantPath.Location = new System.Drawing.Point(219, 25);
+            this.txtValorantPath.Name = "txtValorantPath";
+            this.txtValorantPath.ReadOnly = true;
+            this.txtValorantPath.Size = new System.Drawing.Size(656, 31);
+            this.txtValorantPath.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(34, 155);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "label3";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(142, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Valorant Path";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
             // tabPageLogs
             // 
             this.tabPageLogs.Controls.Add(this.txtBoxLogs);
-            this.tabPageLogs.Location = new System.Drawing.Point(4, 22);
+            this.tabPageLogs.Location = new System.Drawing.Point(8, 39);
+            this.tabPageLogs.Margin = new System.Windows.Forms.Padding(6);
             this.tabPageLogs.Name = "tabPageLogs";
-            this.tabPageLogs.Size = new System.Drawing.Size(520, 484);
+            this.tabPageLogs.Size = new System.Drawing.Size(922, 965);
             this.tabPageLogs.TabIndex = 2;
             this.tabPageLogs.Text = "Logs";
             this.tabPageLogs.UseVisualStyleBackColor = true;
@@ -77,18 +126,20 @@ namespace Valorant__
             // 
             this.txtBoxLogs.Dock = System.Windows.Forms.DockStyle.Fill;
             this.txtBoxLogs.Location = new System.Drawing.Point(0, 0);
+            this.txtBoxLogs.Margin = new System.Windows.Forms.Padding(6);
             this.txtBoxLogs.Multiline = true;
             this.txtBoxLogs.Name = "txtBoxLogs";
             this.txtBoxLogs.ReadOnly = true;
-            this.txtBoxLogs.Size = new System.Drawing.Size(520, 484);
+            this.txtBoxLogs.Size = new System.Drawing.Size(922, 965);
             this.txtBoxLogs.TabIndex = 0;
             // 
             // tabPageClientApi
             // 
             this.tabPageClientApi.Controls.Add(this.txtBoxClientApi);
-            this.tabPageClientApi.Location = new System.Drawing.Point(4, 22);
+            this.tabPageClientApi.Location = new System.Drawing.Point(8, 39);
+            this.tabPageClientApi.Margin = new System.Windows.Forms.Padding(6);
             this.tabPageClientApi.Name = "tabPageClientApi";
-            this.tabPageClientApi.Size = new System.Drawing.Size(461, 500);
+            this.tabPageClientApi.Size = new System.Drawing.Size(922, 965);
             this.tabPageClientApi.TabIndex = 3;
             this.tabPageClientApi.Text = "Client API";
             this.tabPageClientApi.UseVisualStyleBackColor = true;
@@ -97,25 +148,33 @@ namespace Valorant__
             // 
             this.txtBoxClientApi.Dock = System.Windows.Forms.DockStyle.Fill;
             this.txtBoxClientApi.Location = new System.Drawing.Point(0, 0);
+            this.txtBoxClientApi.Margin = new System.Windows.Forms.Padding(6);
             this.txtBoxClientApi.Multiline = true;
             this.txtBoxClientApi.Name = "txtBoxClientApi";
             this.txtBoxClientApi.ReadOnly = true;
-            this.txtBoxClientApi.Size = new System.Drawing.Size(461, 500);
+            this.txtBoxClientApi.Size = new System.Drawing.Size(922, 965);
             this.txtBoxClientApi.TabIndex = 1;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(469, 526);
+            this.ClientSize = new System.Drawing.Size(938, 1012);
             this.Controls.Add(this.tabControl1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(6);
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "Valorant++";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.tabControl1.ResumeLayout(false);
+            this.tabPageSettings.ResumeLayout(false);
+            this.tabPageSettings.PerformLayout();
             this.tabPageLogs.ResumeLayout(false);
             this.tabPageLogs.PerformLayout();
             this.tabPageClientApi.ResumeLayout(false);
@@ -132,6 +191,11 @@ namespace Valorant__
         private System.Windows.Forms.TextBox txtBoxLogs;
         private System.Windows.Forms.TabPage tabPageClientApi;
         private System.Windows.Forms.TextBox txtBoxClientApi;
+        private System.Windows.Forms.TextBox txtValorantPath;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
     }
 }
 
--- Valorant++/Form1.cs
+++ Valorant++/Form1.cs
@@ -1,17 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace Valorant__
 {
     public partial class Form1 : Form
     {
+        private string valorantPath = "-";
+
         public Form1()
         {
             InitializeComponent();
@@ -20,6 +18,126 @@ namespace Valorant__
         private void Form1_Load(object sender, EventArgs e)
         {
             tabControl1.SelectedTab = tabPageLogs;
+            Thread mainThread = new Thread(new ThreadStart(Main));
+            mainThread.IsBackground = true;
+            mainThread.Start();
+        }
+
+        private void Main()
+        {
+            GetSavedPath();
+            IsValorantRunning();
+            NewLog("running...");
+        }
+
+        public delegate void DisplayValorantPathDelegate(string path);
+
+        private void DisplayValorantPath(string path)
+        {
+            if (!this.txtValorantPath.InvokeRequired)
+            {
+                this.txtValorantPath.Text = path;
+            }
+            else
+            {
+                this.Invoke(new DisplayValorantPathDelegate(DisplayValorantPath), new object[] { path });
+            }
+        }
+
+        private void IsValorantRunning()
+        {
+            if (Process.GetProcessesByName("VALORANT").Length < 1)
+            {
+                if(this.valorantPath == "-")
+                {
+                    NewLog("valorant is not running! start valorant or set the valorant path in the settings to start it automatically.");
+                    while (true) ;
+                }
+                else
+                {
+                    try
+                    {
+                        NewLog("starting valorant...");
+                        Process p = new Process();
+                        p.StartInfo.FileName = this.valorantPath;
+                        p.StartInfo.Arguments = "--launch-product=valorant --launch-patchline=live";
+                        p.Start();
+                        int i = 1;
+                        while(Process.GetProcessesByName("VALORANT").Length < 1)
+                        {
+                            i += 1;
+                            if(i > 200)
+                            {
+                                NewLog("valorant has not been detected yet. maybe you specified the wrong valorant path.");
+                                i = 0;
+                            }
+                            Thread.Sleep(100);
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        NewLog("ERROR! cant find " + this.valorantPath);
+                        while (true) ;
+                    }
+                }
+            }
+            NewLog("valorant was successfully detected!");
+            Thread.Sleep(1000);
+        }
+
+        private void GetSavedPath()
+        {
+            try
+            {
+                this.valorantPath = System.IO.File.ReadAllText(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"\Valorant++\settings.txt");
+            }
+            catch (Exception) { };
+            DisplayValorantPath(this.valorantPath);
+        }
+
+        public delegate void NewLogDelegate(string log);
+        private void NewLog(string log)
+        {
+            if (!this.txtBoxLogs.InvokeRequired)
+            {
+                this.txtBoxLogs.Text += ">_ " + log + "\r\n";
+            }
+            else
+            {
+                this.Invoke(new NewLogDelegate(NewLog), new object[] { log });
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "*.exe | *.exe";
+            openFileDialog1.Title = "Valorant++";
+            openFileDialog1.FileName = "";
+            openFileDialog1.InitialDirectory = @"C:\Program Files (x86)\Riot Games\Riot Client";
+            openFileDialog1.ShowDialog();
+            string path = openFileDialog1.FileName;
+
+            if(path != "")
+            {
+                this.valorantPath = path;
+                DisplayValorantPath(this.valorantPath);
+
+                if (!Directory.Exists(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"\Valorant++"))
+                {
+                    Directory.CreateDirectory(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"\Valorant++");
+                }
+
+                using (StreamWriter sw = File.AppendText(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"\Valorant++\settings.txt"))
+                {
+                    sw.Write(path);
+                }
+
+                File.WriteAllText(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"\Valorant++\settings.txt", path);
+
+                NewLog("valorant path changed! please restart valorant++");
+                MessageBox.Show("The path has been changed. Please restart Valorant++", "Valorant++", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
         }
     }
 }
